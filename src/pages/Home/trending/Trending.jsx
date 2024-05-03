8f03a595d8c723c01ea9a52d1a2fff577bff92ed
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-05-03 06:20:49 +0300

    Trending componant and functionality cursoul and rating

--- src/pages/Home/trending/Trending.jsx
+++ src/pages/Home/trending/Trending.jsx
@@ -4,6 +4,7 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import ContentWrepper from "../../../components/contentWrapper/ContentWrepper";
 import { useState } from "react";
 import useFetch from "../../../hooks/useFetch";
+import Carsul from "../../../components/cursoul/Carsul";
 const Trending = () => {
   const [endpoint,setendpoint]=useState("day")
   const {data,loading} = useFetch(`/trending/all/${endpoint}`)
@@ -19,6 +20,7 @@ const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} ontaTabeChange={ontaTabeChange} />
       </ContentWrepper>
+      <Carsul data={data?.results} loading={loading}/>
     </div>
   );
 };
