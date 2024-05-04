ddc549473983b86551674442d87bdbce1bcdec36
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-05-04 10:35:45 +0300

    cast section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import useFetch from "../../hooks/useFetch";
 import { useParams } from 'react-router-dom'; 
 import "./style.scss";
 import DetialsBanner from './DetailsBanner/DetialsBanner';
+import Cast from './cast/Cast';
 function Detailse() {
   const {mediaType,id} = useParams();
   const {data, loading} = useFetch(`/${mediaType}/${id}/videos`);
@@ -11,6 +12,7 @@ function Detailse() {
    
     <div>
       <DetialsBanner  video={data?.resuls?.[0]} crew={credits?.crew}/>
+      <Cast data={credits?.cast} loading={creditsLoading}/>
     </div>
   );
 };
